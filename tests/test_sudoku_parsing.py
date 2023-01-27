985e7a124d836e60e613e287044368581a5663a0
jorgefandinno <jfandinno@unomaha.edu>
2023-01-27 11:55:32 -0600

    update

--- tests/test_sudoku_parsing.py
+++ tests/test_sudoku_parsing.py
@@ -4,17 +4,17 @@ import unittest
 from sudoku_board import Sudoku
 from tests.partial_sudokus import DICTS
 
-INSTANCES = "./instances/"
+INSTANCES = "./instances/txt"
 
 
 class TestFromStr(unittest.TestCase):
-    def assert_sudoku_str(self, sudoku_str):
+    def assert_sudoku_str(self, sudoku_str: str):
         sudoku = Sudoku.from_str(sudoku_str)
         s2 = [line.strip() for line in str(sudoku).strip().splitlines()]
         s3 = [line.strip() for line in sudoku_str.strip().splitlines()]
         self.assertEqual(s2, s3)
 
-    def assert_sudoku_dict(self, sudoku_str, file):
+    def assert_sudoku_dict(self, sudoku_str: str, file: str):
         sudoku = Sudoku.from_str(sudoku_str)
         self.assertEqual(sudoku.sudoku, DICTS[file])
 
